a5ae1416f3a74c6044d5c4eed925c4d6251f15c1
afroz <afrozdd7@gmail.com>
2025-07-22 15:44:18 +0530

    created layout,page,dashboardLayout.jsx files

---
+++ src/components/DashboardLayout.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex h-screen bg-background text-foreground">
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1  p-6 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
+}
