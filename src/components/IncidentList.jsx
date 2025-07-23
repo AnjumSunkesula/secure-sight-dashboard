e032fff81032be490259b055911399394e57b9e5
afroz <afrozdd7@gmail.com>
2025-07-23 19:42:06 +0530

    added icons for timestamp and location

--- src/components/IncidentList.jsx
+++ src/components/IncidentList.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useIncident } from "@/context/IncidentContext";
 import incidents from "@/data/incidents";
+import { DoorOpen, Clock, Cctv } from "lucide-react";
 
 export default function IncidentList() {
   const { setSelectedIncident } = useIncident();
@@ -17,32 +18,45 @@ export default function IncidentList() {
             >
               <img src={`/thumbnails/thumb-${incident.id}.png`} alt="" className="w-full h-[80px] object-cover rounded"/>
             </div>
+
+            {/* Incident Info */}
             <div className=" flex flex-col justify-between">
-              <div className="text-sm font-semibold">{incident.label}</div>
+              <div className="text-sm font-semibold flex gap-1">
+                <DoorOpen className="w-4 h-4 text-red-400"/>
+                {incident.label}
+              </div>
+
+              {/* Incident Location */}
               <div className="text-xs text-gray-400">
-                <div>{incident.location}</div>
-                  <div>
-                    {(() => {
-                      const start = new Date(incident.timestamp);
-                      const end = new Date(start.getTime() + (incident.duration || 0) * 1000);
+                <div className="flex flex-row gap-1">
+                  <Cctv className="w-3.5 h-3.5"/>
+                  {incident.location}
+                </div>
 
-                      const formatTime = (date) => {
-                        const h = String(date.getHours()).padStart(2, '0');
-                        const m = String(date.getMinutes()).padStart(2, '0');
-                        const s = String(date.getSeconds()).padStart(2, '0');
-                        return `${h}:${m}:${s}`;
-                      };
+                {/* Timestamp */}
+                <div className="flex gap-1">
+                  <Clock className="w-3.5 h-3.5"/>
+                  {(() => {
+                    const start = new Date(incident.timestamp);
+                    const end = new Date(start.getTime() + (incident.duration || 0) * 1000);
 
-                      const formatDate = (date) => {
-                        const day = date.getDate();
-                        const month = date.toLocaleString('en-US', { month: 'short' });
-                        const year = date.getFullYear();
-                        return `${day} ${month} ${year}`;
-                      };
+                    const formatTime = (date) => {
+                      const h = String(date.getHours()).padStart(2, '0');
+                      const m = String(date.getMinutes()).padStart(2, '0');
+                      const s = String(date.getSeconds()).padStart(2, '0');
+                      return `${h}:${m}:${s}`;
+                    };
 
-                      return `${formatTime(start)} to ${formatTime(end)} on ${formatDate(start)}`;
-                    })()}
-                  </div>
+                    const formatDate = (date) => {
+                      const day = date.getDate();
+                      const month = date.toLocaleString('en-US', { month: 'short' });
+                      const year = date.getFullYear();
+                      return `${day} ${month} ${year}`;
+                    };
+
+                    return `${formatTime(start)} to ${formatTime(end)} on ${formatDate(start)}`;
+                  })()}
+                </div>
               </div>
             </div>
           </div>
