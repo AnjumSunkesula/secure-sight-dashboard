3a84903182f7819c12bab0eb65f79372f8026a74
afroz <afrozdd7@gmail.com>
2025-07-24 19:23:40 +0530

    modifies for loop from  "i < 12" to "i < 15"

--- prisma/seed.js
+++ prisma/seed.js
@@ -28,7 +28,7 @@ async function main() {
   const now = new Date();
 
   // 2. Create Incidents
-  for (let i = 0; i < 12; i++) {
+  for (let i = 0; i < 15; i++) {
     const start = new Date(now.getTime() - i * 60 * 60 * 1000); // each incident an hour apart
     const end = new Date(start.getTime() + (5 + Math.floor(Math.random() * 20)) * 1000); // 5–25 sec duration
     const camera = [cameraA, cameraB, cameraC][i % 3];
@@ -38,8 +38,8 @@ async function main() {
         type: types[i % types.length],
         tsStart: start,
         tsEnd: end,
-        thumbnailUrl: `/thumbnails/thumb-${i + 1}.png`,
-        videoUrl: `/videos/incident-${i + 1}.mp4`,
+        thumbnailUrl: `/thumbnails/thumb-${(i % 7) + 1}.png`,
+        videoUrl: `/videos/incident-${(i % 7) + 1 }.mp4`,
         camera: {
           connect: { id: camera.id },
         },
