4f2ad38976060c2c499d52c1ddc8f301ec636959
afroz <afrozdd7@gmail.com>
2025-07-24 15:49:29 +0530

    deleted unnecessary files

--- src/app/dashboard/incidents/layout.jsx
+++
@@ -1,9 +0,0 @@
-import { IncidentProvider } from "@/context/IncidentContext";
-
-export default function Layout({ children }) {
-  return (
-    <IncidentProvider>
-      {children}
-    </IncidentProvider>
-  );
-}
