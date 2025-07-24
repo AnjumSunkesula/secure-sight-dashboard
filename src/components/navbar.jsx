986583b2ce560bb30b435ae963a583eaceef1cd3
afroz <afrozdd7@gmail.com>
2025-07-24 21:09:53 +0530

    added a profile pic, a down arrow and no-cursor-allowed for selected elemets

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LayoutDashboard, Cctv, Settings, TriangleAlert, Users } from "lucide-react";
+import { LayoutDashboard, Cctv, Settings, TriangleAlert, Users, ChevronDown } from "lucide-react";
 
 
 export default function Navbar() {
@@ -11,30 +11,36 @@ export default function Navbar() {
       </div>
       <div>
         <nav className="flex gap-6 ml-6 text-sm text-gray-300">
-          <a href="#" className="flex items-center gap-2 hover:text-white">
+          <a href="#" className="flex items-center gap-2 cursor-not-allowed hover:text-white">
             <LayoutDashboard size={16} className="text-yellow-400"/> Dashboard
           </a>
-          <a href="#" className="flex items-center gap-2 hover:text-white">
+          <a href="#" className="flex items-center gap-2 cursor-not-allowed hover:text-white">
             <Cctv size={16} /> Cameras
           </a>
-          <a href="#" className="flex items-center gap-2 hover:text-white">
+          <a href="#" className="flex items-center gap-2 cursor-not-allowed hover:text-white">
             <Settings size={16} /> Scenes
           </a>
           <a href="#" className="flex items-center gap-2 text-white font-bold">
             <TriangleAlert size={16} /> Incidents
           </a>
-          <a href="#" className="flex items-center gap-2 hover:text-white">
+          <a href="#" className="flex items-center gap-2 cursor-not-allowed hover:text-white">
             <Users size={16} /> Users
           </a>
         </nav>
       </div>
       <div className="flex items-center gap-2">
-        <span className="text-sm">Mohammed Ajhas</span>
-        <img
-          src="/user.jpg"
-          alt="User"
-          className="w-8 h-8 rounded-full border border-white"
-        />
+        <div>
+          <img
+            src="/user.jpg"
+            alt="User"
+            className="w-8 h-8 rounded-full border border-white"
+          />
+        </div>
+        <div>
+          <span>Mohammed Ajhas</span>
+          <div className="text-xs text-gray-400">ajhas@mandlac.com</div>
+        </div>
+        <div className="cursor-not-allowed"><ChevronDown /> </div>
       </div>
     </header>
   );
